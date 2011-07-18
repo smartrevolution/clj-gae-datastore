f56adac2ca786f9086c2f52e5a7d10a2b4f49214
Tobias Raeder <tobias.raeder@freiheit.com>
2011-07-18 16:03:03 +0200

    Added cursor based key selection, version 0.3.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-gae-datastore "0.3.5"
+(defproject clj-gae-datastore "0.3.6"
   :description      "clojure dsl for accessing the appengine datastore"
   :dependencies     [[clojure "1.2.0"]
                      [clojure-contrib "1.2.0"]
